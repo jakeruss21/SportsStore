1dc2c2dff227c3ee34335fc08e05c866c4d79f0c
Jacob Russell <jacob.russell82@outlook.com>
2018-10-17 09:07:06 -0400

    adds (weak) pagination to ProductController

--- SportsStore.WebUI/Controllers/ProductController.cs
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -11,7 +11,7 @@ namespace SportsStore.WebUI.Controllers
     public class ProductController : Controller
     {
         private IProductRepository repository;
-
+        public int PageSize = 4;
         // GET: Product
 
         public ProductController(IProductRepository productRepository)
@@ -19,9 +19,11 @@ namespace SportsStore.WebUI.Controllers
             this.repository = productRepository;
         }
 
-        public ViewResult List()
+        public ViewResult List(int page = 1)
         {
-            return View(repository.Products);
+            return View(repository.Products.OrderBy(p=> p.ProductID)
+                        .Skip((page-1)* PageSize)
+                        .Take(PageSize));
         }
     }
 }
