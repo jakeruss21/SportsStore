c4e19eb7032fde22557c667a41a6d1dc501e0874
Jacob Russell <jacob.russell82@outlook.com>
2018-10-23 08:57:40 -0400

    Adds unit testing for 'selected category view'

--- SportsStore.UnitTests/UnitTest1.cs
+++ SportsStore.UnitTests/UnitTest1.cs
@@ -2,6 +2,7 @@
 using System.Web;
 using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.CSharp;
 using System.Collections.Generic;
 using System.Linq;
 using Moq;
@@ -125,6 +126,31 @@ namespace SportsStore.UnitTests
             Assert.AreEqual(results[0], "Apples");
             Assert.AreEqual(results[1], "Oranges");
             Assert.AreEqual(results[2], "Plums");
+        }
+
+        [TestMethod]
+        public void Indicates_Selected_Category()
+        {
+            //Arrange
+            // - create the mock repository
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product {ProductID = 1, Name = "P1", Category = "Apples" },
+                new Product {ProductID = 4, Name = "P2", Category = "Oranges" }
+            });
+
+            //Arrange - creates the controller
+            NavController target = new NavController(mock.Object);
+
+            //Arrange - define the category to selected string categoryToSelect = "Apples"
+            string categoryToSelect = "Apples";
+
+            //Action
+            string result = target.Menu(categoryToSelect).ViewBag.SelectedCategory;
+
+            //Assert
+            Assert.AreEqual(categoryToSelect, result);
         }
     }
 }
