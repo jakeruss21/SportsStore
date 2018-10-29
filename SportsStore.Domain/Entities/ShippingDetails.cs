2ebe8157c474b11905b422a22e241873705e1f94
Jacob Russell <jacob.russell82@outlook.com>
2018-10-29 09:26:43 -0400

    view for the checkout shipping information

--- SportsStore.Domain/Entities/ShippingDetails.cs
+++ SportsStore.Domain/Entities/ShippingDetails.cs
@@ -7,7 +7,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace SportsStore.Domain.Entities
 {
-    class ShippingDetails
+    public class ShippingDetails
     {
         [Required(ErrorMessage ="Please enter a name")]
         public string Name { get; set; }
