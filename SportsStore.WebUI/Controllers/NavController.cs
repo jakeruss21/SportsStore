0b84d7b305a60d626160d26b223b794b81ef05e2
Jacob Russell <jacob.russell82@outlook.com>
2018-11-05 09:17:11 -0500

    change layout of the sort by category buttons for mobile display.

--- SportsStore.WebUI/Controllers/NavController.cs
+++ SportsStore.WebUI/Controllers/NavController.cs
@@ -16,7 +16,7 @@ namespace SportsStore.WebUI.Controllers
             repository = repo;
         }
 
-        public PartialViewResult Menu(string category = null)
+        public PartialViewResult Menu(string category = null, bool horizontalLayout = false)
         {
             ViewBag.SelectedCategory = category;
 
@@ -24,8 +24,8 @@ namespace SportsStore.WebUI.Controllers
                                     .Select(x => x.Category)
                                     .Distinct()
                                     .OrderBy(x => x);
-
-            return PartialView(categories);
+            string viewName = horizontalLayout ? "MenuHorizontal" : "Menu";
+            return PartialView(viewName, categories);
         }
     }
 }
