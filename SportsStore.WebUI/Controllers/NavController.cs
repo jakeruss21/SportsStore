775f02d5bb17509a23b539bd90069a352b274ebd
Jacob Russell <jacob.russell82@outlook.com>
2018-11-05 09:56:35 -0500

    fixes the duplicate Menu types into one view class instead of two

--- SportsStore.WebUI/Controllers/NavController.cs
+++ SportsStore.WebUI/Controllers/NavController.cs
@@ -16,7 +16,7 @@ namespace SportsStore.WebUI.Controllers
             repository = repo;
         }
 
-        public PartialViewResult Menu(string category = null, bool horizontalLayout = false)
+        public PartialViewResult Menu(string category = null)
         {
             ViewBag.SelectedCategory = category;
 
@@ -24,8 +24,8 @@ namespace SportsStore.WebUI.Controllers
                                     .Select(x => x.Category)
                                     .Distinct()
                                     .OrderBy(x => x);
-            string viewName = horizontalLayout ? "MenuHorizontal" : "Menu";
-            return PartialView(viewName, categories);
+            
+            return PartialView("FlexMenu", categories);
         }
     }
 }
